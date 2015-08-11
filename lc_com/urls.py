8e8d25de98beb0e447427f234bde752c0f5f5957
jackwhit3 <josh7910@gmail.com>
2015-08-11 19:36:27 +0800

    update home content, add projects/contact pages and icon

--- lc_com/urls.py
+++ lc_com/urls.py
@@ -19,5 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^projects', TemplateView.as_view(template_name='projects.html')),
+    url(r'^contact', TemplateView.as_view(template_name='contact.html')),
 	url(r'^', TemplateView.as_view(template_name='home.html')),
 ]
