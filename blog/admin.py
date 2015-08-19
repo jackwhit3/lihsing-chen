a306804d9fef8ca502b876564040592b94e376a8
jackwhit3 <josh7910@gmail.com>
2015-08-19 22:59:24 +0800

    add ckeditor for admin page

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
 from .models import Post
+#below two are for ckeditor
+from ckeditor.widgets import CKEditorWidget
+from django import forms
+
+
+class PostAdminForm(forms.ModelForm):
+	body = forms.CharField(widget=CKEditorWidget())
+	class Meta:
+		model = Post
+		fields = '__all__'
 
 class PostAdmin(admin.ModelAdmin):
-
 	fields = ['title','slug','pub_date','body']		#the order of attributes of models to display
 	prepopulated_fields = {"slug": ("title",)}
 	list_display = ('title','pub_date','body')				#what attributes to show on admin panel
 	list_filter = ['pub_date']
 	search_fields = ['title','body']
-
+	form = PostAdminForm
 
 admin.site.register(Post, PostAdmin)
--- lc_com/urls.py
+++ lc_com/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     url(r'^projects', TemplateView.as_view(template_name='projects.html')),
     url(r'^contact', TemplateView.as_view(template_name='contact.html')),
     url(r'^blog/', include('blog.urls')),
+	url(r'^ckeditor/', include('ckeditor.urls')),
 	url(r'^', TemplateView.as_view(template_name='home.html')),
 ]
